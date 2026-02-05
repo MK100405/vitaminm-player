539774a2bc3ce518a9b79a37aee8b102a9749e37
MK100405 <allsrightwiththeworld@gmail.com>
2026-02-05 13:56:24 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "vitaminm-pwa-v2";
+
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./title.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
